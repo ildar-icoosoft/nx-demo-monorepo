0c724a84423287aaa17f54bc01ce7920bb59133c
Ildar Farrakhov <ildar.icoosoft@gmail.com>
2022-07-03 04:40:19 +0500

    refactor: Удалил лишний компонент, который был создан при установке

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from "@angular/common/http";
 import { UiModule } from "@nx-demo-monorepo/ui";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, UiModule],
   providers: [],
   bootstrap: [AppComponent],
